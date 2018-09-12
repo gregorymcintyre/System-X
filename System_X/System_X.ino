b8f77ef3d1c3c46803fb4b2419d52e88a84dfd7b
Greg McIntyre <gregmcintyre1984@gmail.com>
2018-09-12 11:45:26 +1000

    version 1.0

--- System_X/System_X.ino
+++ System_X/System_X.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+/* System_X.ino
+ *  
+ *  Greg MCIntyre
+ *  
+ *  using three sensors overlapping arc to improve detection and reduce false positives
+ * 
+ */
 
+void setup() {
+  //configure for three HCSR505 IR sensors
+  pinMode(3, INPUT);
+  pinMode(5, INPUT); //centre sensor
+  pinMode(6, INPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // 
+
+  if ((digitalRead(3) == HIGH && digitalRead(5) == HIGH) || (digitalRead(5) == HIGH && digitalRead(6) == HIGH))
+  {
+    digitalWrite(LED_BUILTIN, HIGH); //on for alert
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW); //off for alert
+  }
+      
 
 }
